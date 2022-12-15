c649fad74418ef85be7598ddc21aea7a66319789
Андрюшенкова Елизавета <120619101+elixxa@users.noreply.github.com>
2022-12-15 18:44:53 +0300

    Add files via upload

---
+++ learnPython-master/learnPython-master/DevOps/practice_4.py
@@ -0,0 +1,230 @@
+﻿import os
+from datetime import datetime
+import hashlib
+
+
+def print_details(a):
+    print(
+        f"{a=}\ta.id: {id(a)}\ntype of a:{type(a)}\nis a an object:{isinstance(a, object)}\n"
+    )
+
+
+def fn2():
+    pass
+
+
+class A:
+    name = "cls_name"
+    __cls_private = "cls_private"
+    _cls_protected = "cls_protected"
+
+    def __init__(self, val):
+        self.val = val
+        self._protected = "protected"
+        self.__private = "private"
+
+    def print(self):
+        print(
+            f"{self.val=}, {self._protected=}, {self.__private=}, "
+            f"{self.name=}, {self.__cls_private=}"
+        )
+
+
+class B(A):
+    def __init__(self, val):
+        super().__init__(val)
+
+    def __str__(self):
+        return f"< Class instance of B. {self.val=} >"
+
+    def multiply(self, *args):
+        if len(args) > 0:
+            res = self.val
+            for arg in args:
+                try:
+                    print(f"Evaluating {res}*{arg} ...")
+                    res *= arg
+                except Exception as ex:
+                    print(
+                        f"Something gone wrong at multipliing {res } and {arg}\n{str(ex)}"
+                    )
+            return res
+        else:
+            try:
+                return self.val * self.val
+            except ValueError as ex:
+                print(f"Can't multiple {self.val} course it's {type(self.val)}")
+                return None
+
+
+###########################################Class methods################################################################
+class MethodDifference:
+    class_attr = "Only class methods can change me."
+
+    @staticmethod
+    def print_static():
+        print("static")
+
+    @classmethod
+    def print_cls(cls):
+        print(f"class_method for {cls.__name__}")
+
+    @classmethod
+    def change_attr(cls, new_val):
+        cls.class_attr = new_val
+        print(f"Changed! {cls.class_attr=}")
+
+    def __init__(self, val):
+        self.val = val
+
+    def print_offset(self, offset=10):
+        print(self.val + offset)
+
+    def __str__(self):
+        return f"{self.__class__.__name__}:val={self.val}"
+
+
+class VanillaPyUser:
+    def __init__(self, name, password):
+        self.__name = name
+        self.password_hash = None
+        self.password = password
+
+    @staticmethod
+    def make_hash(data: str) -> str:
+        """method that emulates hashing. IRL u need real hasher(seed, val2hash)"""
+        hashStr = hashlib.md5(data.encode()).hexdigest()
+        print(f"Hash is {hashStr}")
+        return hashStr
+
+    @property
+    def name(self):
+        """name is read-only"""
+        return self.__name
+
+    @property
+    def password(self):
+        raise AttributeError("Password is write-only")
+
+    @password.setter
+    def password(self, plaintext):
+        self.password_hash = VanillaPyUser.make_hash(plaintext)
+
+
+##################################################Class#Inheritance#####################################################
+class Ancestor:
+    pass
+
+
+class FirstGen(Ancestor):
+    pass
+
+
+class AnotherFirstGen:
+    pass
+
+
+class Child(FirstGen, AnotherFirstGen):
+    pass
+
+
+##################################################Class#IRL#############################################################
+class Alive:
+    def die(self):
+        print(f"{self} says 'Im dead!'")
+
+    def fly(self):
+        print(f"{self} says 'Lets try flying. It's safe and fun'")
+        self.die()
+
+
+class Duck(Alive):
+    def answer(self):
+        print(f"{self} says 'Quack!'")
+
+    def fly(self):
+        print(f"{self} says 'Easy! I`m flying'")
+
+
+class Developer(Alive):
+    def develop(self):
+        print(f"{self} says 'WIP. Reading Manual!'")
+
+    def die(self):
+        print(f"{self} says: `X.X & v prodakshen. Oops...`")
+
+    def answer(self):
+        print(f"{self} says 'Not now i`m busy, reading manual!'")
+
+
+# class DuckDeveloper(Developer, Duck):
+class DuckDeveloper(Duck, Developer):
+    pass
+
+
+if __name__ == "__main__":
+    
+    # Everything is an Object, one more time with feeling
+    # print_details(a=1)
+    # print_details(a="Hello world!")
+    # print_details(a=datetime.now())
+    # print_details(a=os.path.realpath("./"))
+    # print_details(fn2)
+    # a = A(4834)
+    # a.print()
+    # a=A(322)
+    # b=A("sdsafafsa")
+    # print_details(a)
+    # a.print()
+    # print_details(b)
+    # b.print()
+    # print("Lets try an instance of our class c= B(322)")
+    # c=B(88889999) #Можно брейкпоинт тут поставить, чтобы закончить все ращговоры за инкапсуляцию. Т.е. если вы в лагере ее нет, вот пруф.
+    # print_details(c)
+    # print(c.multiply())
+    # print(c.multiply(1,2,3))
+
+    # print("\nAnd another one: d = B(3)")
+    # d = B(3)
+    # print(d.multiply(91))
+    # print(d.multiply("Privet, KAK DILA? "))
+    # print(d.multiply("Privet,", "KAK DILA? "))
+
+    # Not all Methods are the same
+    # print(f"No instance yet. But i can call static and class methods:")
+    # MethodDifference.print_cls()
+    # MethodDifference.print_static()
+    # md_inst = MethodDifference(3)
+    # print(md_inst)
+    # md_inst.print_offset()
+    # md_inst.print_offset(10)
+
+    # Getters setters in vanilla python style
+    # usr1 = VanillaPyUser("user1", "funnypass")
+    # print(usr1.name)
+    # try:
+    #     print(usr1.password)
+    # except AttributeError as ex:
+    #     print(str(ex))
+    # usr1.password = "LessFunnyPass"
+
+    # Inheritance101: MRO and bases
+    # print(FirstGen.__bases__)
+    # print(FirstGen.mro())
+    # print(Child.__bases__)
+    # print(Child.mro())
+    # print(2+2)
+    # MRO and Ducks in oneshot. Need to change DuckDeveloper definiton...
+    ducky = Duck()
+    ducky_dev = DuckDeveloper()
+    dev = Developer()
+
+    ducky.answer()
+    ducky.die()
+    dev.answer()
+    dev.die()
+    ducky_dev.answer()
+    ducky_dev.die()
+    print(DuckDeveloper.mro())
+    # Идем в объявление DuckDeveloper. Меняем порядок. Наша утка-разраб становится сначала (более) деволер, чем утка.
+    # class DuckDeveloper(Duck, Developer): --> class DuckDeveloper(Developer, Duck):
---
+++ learnPython-master/learnPython-master/DevOps/practice_6.py
@@ -0,0 +1,310 @@
+﻿# Imports wich demonstrate how imports work
+# import this
+from config import *
+from commonNS import CommonNS
+
+import os
+# from os import path
+# from os import path as pt
+# from os import *
+
+import random
+import inspect
+import string
+import csv
+import json
+from urllib import request
+from jinja2 import Template, Environment, FileSystemLoader
+
+###############################################CSV101###################################################################
+def test_csv():
+    data = [
+        (1, "A towel,", 1.0),
+        (42, " it says, ", 2.0),
+        (1337, "is about the most ", -1),
+        (0, "massively useful thing ", 123),
+        (-2, "an interstellar hitchhiker can have.", 3),
+    ]
+
+    # Write CSV file
+    with open("./devops/input/test.csv", "wt", newline="") as fp:
+        writer = csv.writer(fp, delimiter=",")
+        # writer.writerow(["your", "header", "foo"])  # write header
+        writer.writerows(data)
+
+    # Read CSV file
+    with open("./devops/input/test.csv") as fp:
+        reader = csv.reader(fp, delimiter=",", quotechar='"')
+        # next(reader, None)  # skip the headers
+        data_read = [row for row in reader]
+    print(data_read)
+
+
+def minimal_csv_reader(fpath="./devops/input/color_web_safe.csv"):
+    with open(fpath, "r") as file:
+        # with open(fpath, "r") as fh:
+        reader = csv.reader(file)
+        for row in reader:
+            print(row[0], " - ", row[1])
+
+
+def random_char_seq(len):
+    lst = string.ascii_letters
+    return "".join(random.choice(lst) for _ in range(len))
+
+
+def minimal_writer(
+    fpath="./devops/input/color_web_safe.csv", amount=10, ch_len=16, verb=False
+):
+    with open(fpath, "wt") as wfh:
+        writer = csv.writer(wfh)
+        writer.writerow(("ID", "Random", "Date", "Unicode"))  # Just a tutple
+        for idx in range(1, amount + 1):
+            row = (
+                idx,
+                os.urandom(ch_len),
+                "{:02d}.12.22".format(11 + idx),
+                random_char_seq(ch_len),
+            )
+            writer.writerow(row)
+    if verb:
+        print(open(fpath, "rt").read())
+
+
+def dialects_101():
+    print(csv.list_dialects())
+    csv.register_dialect(
+        "escaped",
+        escapechar="\\",
+        doublequote=False,
+        quoting=csv.QUOTE_NONE,
+    )
+    print(csv.list_dialects())
+
+
+def dialect_next():
+    csv.register_dialect(
+        "singlequote",
+        quotechar="'",
+        quoting=csv.QUOTE_ALL,
+    )
+    TEMPLATE = """\
+        Dialect: "{name}"
+
+        delimiter   = {dl!r:<6}    skipinitialspace = {si!r}
+        doublequote = {dq!r:<6}    quoting_modes_existed in python <= 3.6
+        quotechar   = {qc!r:<6}    lineterminator   = {lt!r}
+        escapechar  = {ec!r:<6}
+    """
+    for name in sorted(csv.list_dialects()):
+        dialect = csv.get_dialect(name)
+
+        print(
+            TEMPLATE.format(
+                name=name,
+                dl=dialect.delimiter,
+                si=dialect.skipinitialspace,
+                dq=dialect.doublequote,
+                qc=dialect.quotechar,
+                lt=dialect.lineterminator,
+                ec=dialect.escapechar,
+            )
+        )
+
+        # такое себе, но если у writer`ов есть стдоут мод, будем его в этой лабе писать...
+        writer = csv.writer(sys.stdout, dialect=dialect)
+        writer.writerow(
+            (
+                "col1",
+                1,
+                "10/01/2010",
+                "Special chars: \" ' {} to parse".format(dialect.delimiter),
+            )
+        )
+        print()
+
+
+def dict_style_csv(fpath="./devops/output/dict_wr_csv.csv"):
+    fieldnames = ("Title 1", "Title 2", "Title 3", "Title 4")
+    headers = {n: n for n in fieldnames}
+
+    with open(fpath, "wt") as f:
+        writer = csv.DictWriter(f, fieldnames=fieldnames)
+        writer.writeheader()
+
+        for i in range(3):
+            writer.writerow(
+                {
+                    "Title 1": i + 1,
+                    "Title 2": chr(ord("a") + i),
+                    "Title 3": "08/{:02d}/07".format(i + 1),
+                    "Title 4": random_char_seq(5),
+                }
+            )
+
+    print(open(fpath, "rt").read())
+
+
+##############################Bonus CSV Trashy example csv->dict->jinja->HTML###########################################
+def swatches_from_csv(fpath="./devops/input/color_web_safe.csv"):
+    swatch_list = []
+
+    with open(fpath, "rt") as fh:
+        reader = csv.reader(fh)
+        for idx, row in enumerate(reader):
+            d = {}
+            d["id"] = f"swatch{idx}"
+            d.update({"hex": row[0], "rgb": row[1]})
+            swatch_list.append(d)
+
+    jinja_loader = FileSystemLoader("./input/templates/")
+    env = Environment(loader=jinja_loader)
+    template = env.get_template("swatches.html")
+    output = template.render(swatchl=swatch_list)
+    print(output)
+
+    with open("./output/swatches.html", "wt") as wfh:
+        wfh.write(output)
+
+
+###################################################JSON#################################################################
+def json_101():
+    data = [{"a": "A", "b": (2, 4), "c": 3.0}]
+    print("DATA   :", data)
+
+    data_string = json.dumps(data)
+    print("ENCODED:", data_string)
+
+    decoded = json.loads(data_string)
+    print("DECODED:", decoded)
+
+    print("ORIGINAL:", type(data[0]["b"]))
+    print("DECODED :", type(decoded[0]["b"]))
+
+    # тут выкосячивает лишний пробел, ибо не формат строка. мне уже лень чинить - устал.
+    print("PRETTY_PRINT:\n", json.dumps(data[0], sort_keys=True, indent=2))
+
+
+def json_and_dicts():
+    data = [{"a": "A", "b": (2, 4), "c": 3.0, ("d",): "D tuple"}]
+
+    print("First attempt")
+    try:
+        print(json.dumps(data))
+    except TypeError as err:
+        print("ERROR:", err)
+
+    print()
+    print("Second attempt")
+    print(json.dumps(data, skipkeys=True))
+
+
+class EncoderExample(json.JSONEncoder):
+    def default(self, obj):
+        print("default(", repr(obj), ")")
+        # Convert objects to a dictionary of their representation
+        d = {
+            "__class__": obj.__class__.__name__,
+            "__module__": obj.__module__,
+        }
+        d.update(obj.__dict__)
+        return d
+
+
+class ObjectToEncode:
+    def __init__(self, argdata, *args, **kwargs):
+        self.name = "Petr"
+        self.surname = "Morozov"
+        self.list_data = [x for x in range(1, 10)]
+        self.dict_data = dict.fromkeys([x for x in range(1, 10)], 1)
+        self.argdata = argdata
+        self.process_kwargs(args)
+    def __repr__(self):
+        return (
+            f" <ObjectToEncode> {self.surname=} {self.argdata=}\n"
+            f"{self.dict_data=}\n{self.list_data=}"
+        )
+    def process_kwargs(self, *args, **kwargs):
+        pass
+
+def encode_custom_object(obj):
+    encoder = EncoderExample()
+    # default принтит, паралельно с енкодом
+    print("Our obj __repr__")
+    encoded_json = encoder.encode(obj)
+    print("ENCODED:")
+    restored = json.loads(encoded_json)
+    print(json.dumps(restored, sort_keys=True, indent=2))
+    print(f"TYPES: {type(obj)} {type(encoded_json)} {type(restored)}")
+    # за декодеры объяснять не будем, потому что там таже проблема но в квадрате.
+    # т.е. без рефлекта не объяснишь как он работает. Ну и понимать наследование надо.
+##############################################requests#####################################################################
+
+def vanilla_requests_101(url_str="https://ya.ru", path2data="./output/data.html"):
+    response = request.urlopen(url_str)
+    print('URL     :', response.geturl())
+    headers = response.info()
+    print('DATE    :', headers['date'])
+    print('HEADERS :')
+    print('---------')
+    print(headers)
+    data = response.read().decode('utf-8')
+    print('LENGTH  :', len(data))
+    ### в дате лежит тело. но мы упарываемся об капчу...
+    with open(path2data, 'wt') as wfh:
+        wfh.write(data)
+
+#Не заработает без токена и локал хоста...
+def request_with_query_2(url_str="'http://localhost:8080/'", query_args={'q': 'query string', 'foo': 'bar'}):
+    encoded_args = parse.urlencode(query_args).encode('utf-8')
+    print(request.urlopen(url, encoded_args).read().decode('utf-8'))
+
+def post_query_2(url_str="'http://localhost:8080/'", query_args={'q': 'query string', 'foo': 'bar'}):
+    r = request.Request(url=url_str, data=parse.urlencode(query_args).encode('utf-8'),)
+    r.add_header('User-agent','python-script',)
+    response = request.urlopen(r).read().decode('utf-8')
+
+def js_requests():
+    token = os.environ.get("GITHUB_TOKEN")
+
+
+if __name__ == "__main__":
+    cfg = Config("00:00")
+    cfg.print_status_time()
+    # print_secret_key()
+    # print(f"Hello, from {__name__}!")
+
+    # print(os.path.realpath('./'))
+    # print(path.realpath('./'))
+    # print(pt.realpath('/'))
+
+    # Imports syspath hack #чтоб его показать надо pip uninstall сделать, потому что у вас пакет уже поставлен пипом.
+    # sys.path.insert(1, os.path.join(sys.path[0], '..'))
+    # import file_generator
+    # print("modules")
+
+    # cns =CommonNS()
+    # # print(cns.current_fn_name())
+    # # print(cns.current_caller_name())
+    # d1= { 1:4444, 2:5555}
+    # d2 = { 3:3333}
+    # print(cns.merge_dicts(d1,d2))
+
+    ##########################################  CSV  ###################################################################
+    # test_csv()
+
+    # minimal_csv_reader()
+    # minimal_writer(verb=True)
+
+    # dialects_101()
+    # dialect_next()
+    # dict_style_csv()
+    # swatches_from_csv()
+    ##########################################  JSON  ##################################################################
+    # json_101()
+    # json_and_dicts()
+    someObj = ObjectToEncode("Some Data")
+    encode_custom_object(someObj)
+    
+    ############################################  REQUESTS  #############################################################
+    # vanilla_requests_101()
